33cd05e2db852c2cf8a220a0d28c4efca5f47555
Atharva Behani <atharva2204@gmail.com>
2025-02-06 18:21:56 +0000

    Updated API_BASE_URL to be used as an environment variable

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
-const API_BASE_URL = "https://claims-management-system-gpit.onrender.com"; // Backend URL
-// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Fetch from environment variables
 
 export default API_BASE_URL;
